16a48b7c33fcf952be477344f227e26b54814703
Tsukasa353 <71914684+Tsukasa353@users.noreply.github.com>
2020-11-22 13:12:36 +0800

    Add files via upload

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+	entry: "./App/index.js",
+	output: {
+		path: path.resolve(__dirname, "Dist"),
+		filename: "bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: "style-loader",
+					},
+					{
+						loader: "css-loader",
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new htmlWebpackPlugin({
+			template: "./App/index.html",
+			filename: "index.html",
+		}),
+		new htmlWebpackPlugin({
+			template: "./App/absen.html",
+			filename: "absen.html",
+		}),
+	],
+};
